c5d0f4fdf1624014ba3b0a856ff517b165fd3f73
achyuthreddy6363 <achyuthreddy6363@gmail.com>
2022-02-02 13:33:46 +0530

    Electron_framework

---
+++ src/components/File.js
@@ -0,0 +1,17 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+const File = () => {
+    return(
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>React Electron</h1>
+          <p>Files</p>
+        </header>
+      </div>
+
+    )
+
+}
+export default File 
