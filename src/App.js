406bf67f28bc2db2b12312b6ab89cc3dfe7a53e3
achyuthreddy6363 <achyuthreddy6363@gmail.com>
2022-02-03 10:55:54 +0530

    Snipping_Tool

--- src/App.js
+++ src/App.js
@@ -3,21 +3,59 @@ import './App.css';
 import { Navbar, Button, Alignment, Icon } from '@blueprintjs/core';
 
 function App() {
+  const onSnipClick = async () => {
+    const { desktopCapturer, shell, remote } = window.require('electron');
+    const screen = remote.screen;
+    const path = window.require('path');
+    const os = window.require('os');
+    const fs = window.require('fs');
+    const win = remote.getCurrentWindow();
+    const windowRect = win.getBounds();
 
-const onSnipClick = async () => {
-    // In the main process.
-const { desktopCapturer } = require('electron')
-//const { ipcRenderer } = require('electron')
+    win.hide();
 
-desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
-  for (const source of sources) {
-    if (source.name === 'Electron') {
-      mainWindow.webContents.send('SET_SOURCE', source.id)
-      return
+    try {
+      const screenSize = screen.getPrimaryDisplay().workAreaSize;
+      const maxDimension = Math.max(screenSize.width, screenSize.height);
+
+      const sources = await desktopCapturer.getSources({
+        types: ['screen'],
+        thumbnailSize: {
+          width: maxDimension * window.devicePixelRatio,
+          height: maxDimension * window.devicePixelRatio
+        }
+      });
+
+      const entireScreenSource = sources.find(
+        source => source.name === 'Entire Screen' || source.name === 'Screen 1'
+      );
+
+      if (entireScreenSource) {
+        const outputPath = path.join(os.tmpdir(), 'screenshot.png');
+
+        const image = entireScreenSource.thumbnail
+          .resize({
+            width: screenSize.width,
+            height: screenSize.height
+          })
+          .crop(windowRect)
+          .toPNG();
+
+        fs.writeFile(outputPath, image, err => {
+          // win.show();
+
+          if (err) {
+            return console.error(err);
+          }
+          shell.openExternal(`file://${outputPath}`);
+        });
+      } else {
+        window.alert('Screen source not found.');
+      }
+    } catch (err) {
+      console.error(err);
     }
-  }
-);
-}
+  };
 
   return (
     <div className="App">
